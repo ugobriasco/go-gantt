06d1de925dad5fd17bf6ad26575d72b2e3406774
Ugo Briasco <ugo.briasco@gmail.com>
2017-02-25 20:04:41 +0100

    readme cleaned up

--- src/assets/js/youtubecover.js
+++ src/assets/js/youtubecover.js
@@ -1,4 +1,4 @@
-var tag = document.createElement('script');
+var tag = document.createElement('script')
 		tag.src = 'https://www.youtube.com/player_api';
 var firstScriptTag = document.getElementsByTagName('script')[0];
 		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
