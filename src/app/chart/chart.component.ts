6154ba36015e44ef520444c5a23f0c82ae518413
Ugo Briasco <ugo.briasco@gmail.com>
2017-02-28 22:26:23 +0100

    graph rendering

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -14,8 +14,7 @@ chart = {
   chartType: 'Gantt',
   dataTable: [
 ['Task ID', 'Task Name', 'Start Date', 'End Date', 'Duration', 'Percent Complete', 'Deependencies'],
-['1', 'Phase 1', new Date(2016,0,1), new Date(2016,0,2), 200, 100, null],
-['2', 'Phase 2', new Date(2016,0,2), new Date(2016,0,3), null, 20, '1'],
+
 
 
 ],
@@ -27,25 +26,26 @@ chart = {
 
 task = new Task();
 
-counter = 2;
+counter = this.chart.dataTable.length-1;
 
 
 
   constructor() { }
 
   ngOnInit() {
-  	console.log(this.chart.dataTable);
+  	console.log(this.counter);
   	//this.task.id = this.chart.dataTable[this.chart.dataTable.length-1].id
   }
 
 
   //submit
   onSubmit(){
-  	this.task.start = new Date(this.task.start);
-    this.task.end = new Date(this.task.end);
+  	this.task.start = new Date(this.stringToDate(this.task.start));
+    this.task.end = new Date(this.stringToDate(this.task.end));
     this.counter = this.counter+1;
-    this.task.id = this.counter;
-  	this.chart.dataTable.push(this.TasktoArray(this.task));
+    this.task.id = this.counter.toString();
+  	this.chart.dataTable.push(this.TaskToArray(this.task));
+    this.chart = Object.create(this.chart);
     
     
 
@@ -53,13 +53,10 @@ counter = 2;
 
   }
 
-
   //select
 
-
   //convert json to array
-
-  TasktoArray($task: Task){
+  TaskToArray($task: Task){
 
   	let result = [];
 
@@ -70,8 +67,11 @@ counter = 2;
   	// conversione in date
 
   	return result;
+  }
 
-
+  stringToDate(str: string){
+    let parts = str.split('-');
+    return new Date(parts[2],parts[0]-1,parts[1]);
   }
 
 
