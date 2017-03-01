83e0859ef2ed0066bcb632a952355b8cdffa8085
Ugo Briasco <ugo.briasco@gmail.com>
2017-03-01 22:06:02 +0100

    replacing hrefs with routerLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes} from '@angular/router';
+
 import { Ng2GoogleChartsModule } from 'ng2-google-charts';
 
 
